997bdc4618efa2392277c9f988f70c58b5182621
Justin Poliachik <jpoliachik@gmail.com>
2020-03-08 19:12:02 -0400

    Testing node and piñata upload

---
+++ node/uploadPinata.js
@@ -0,0 +1,50 @@
+//imports needed for this function
+const axios = require("axios");
+const fs = require("fs");
+const FormData = require("form-data");
+const recursive = require("recursive-fs");
+const basePathConverter = require("base-path-converter");
+
+export const pinDirectoryToIPFS = (pinataApiKey, pinataSecretApiKey) => {
+  const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+  const src = "../public";
+
+  //we gather the files from a local directory in this example, but a valid readStream is all that's needed for each file in the directory.
+  recursive.readdirr(src, function(err, dirs, files) {
+    let data = new FormData();
+    files.forEach(file => {
+      //for each file stream, we need to include the correct relative file path
+      data.append(`file`, fs.createReadStream(file), {
+        filepath: basePathConverter(src, file),
+      });
+    });
+
+    const metadata = JSON.stringify({
+      name: "justinpoliachik.com",
+    });
+    data.append("pinataMetadata", metadata);
+
+    return axios
+      .post(url, data, {
+        maxContentLength: "Infinity", //this is needed to prevent axios from erroring out with large directories
+        headers: {
+          "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
+          pinata_api_key: pinataApiKey,
+          pinata_secret_api_key: pinataSecretApiKey,
+        },
+      })
+      .then(function(response) {
+        //handle response here
+        console.log(response);
+        console.log("DONE!");
+      })
+      .catch(function(error) {
+        //handle error here
+        throw new Error(error);
+      });
+  });
+};
+
+const pinataApiKey = process.env.PINATA_API_KEY;
+const pinataSecretApiKey = process.env.PINATA_API_SECRET_KEY;
+pinDirectoryToIPFS(pinataApiKey, pinataSecretApiKey);

28603a30e6dc4cafa0b227fc433d9dfb6695e3b0
Justin Poliachik <jpoliachik@gmail.com>
2022-11-23 14:28:44 -0500

    recent creations grid layout

--- components/RecentCreations.tsx
+++ components/RecentCreations.tsx
@@ -15,18 +15,23 @@ export const RecentCreations = ({ creationsList }: RecentCreationProps) => {
       <div className="grid grid-cols-4 gap-4">
         {firstFour.map((creation) => {
           return (
-            <a href={"/"} className="rounded-xl overflow-hidden bg-slate-200 relative">
-              <Image
-                alt={creation.title || "creation"}
-                fill={true}
-                // width={200}
-                // height={200}
-                src={creation.images[0]}
-                style={{ objectFit: "contain", overflow: "hidden", aspectRatio: "1" }}
-              />
-            </a>
+            <div className="relative rounded-xl overflow-hidden bg-slate-200 aspect-square">
+              <a className="hover:opacity-70" href="/">
+                <Image
+                  alt={creation.title || "creation"}
+                  fill={true}
+                  src={creation.images[0]}
+                  style={{ objectFit: "cover" }}
+                />
+              </a>
+            </div>
           );
         })}
+      </div>
+      <div className="text-right mt-2">
+        <a href="/creations" className="text-l font-sans font-semibold text-green-700 no-underline hover:underline">
+          All Creations
+        </a>
       </div>
     </div>
   );

5eb8eca83ca9480d5ef7aba9ddb0888b7edb978e
Justin Poliachik <jpoliachik@gmail.com>
2022-12-05 18:49:37 -0500

    add tiktok to social links

--- components/MainFooter.tsx
+++ components/MainFooter.tsx
@@ -5,7 +5,7 @@ export const MainFooter = () => {
   return (
     <div className="flex h-full flex-row items-center justify-center">
       <div className="text-xs text-gray-400 mr-8">@ Justin Poliachik ~ all of the years ~</div>
-      <SocialLinks />
+      <SocialLinks showResume={true} />
       <a className="ml-4 text-sm font-semibold text-green-700 no-underline hover:underline" href={resumeUrl}>
         CV (2022)
       </a>
--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -2,29 +2,35 @@ import Twitter from "../icons/twitter.svg";
 import GitHub from "../icons/github.svg";
 import YouTube from "../icons/youtube.svg";
 import LinkedIn from "../icons/linkedin.svg";
+import TikTok from "../icons/tiktok.svg";
 import Document from "../icons/document.svg";
 import { resumeUrl } from "../lib/links";
 
 const fillColor = "#262626";
 
-export const SocialLinks = () => {
+export const SocialLinks = (props: { showResume?: boolean }) => {
   return (
     <div className="flex flex-row space-x-4">
       <a href="https://twitter.com/JPoliachik">
         <Twitter className="svg-link w-5 h-5" fill={fillColor} />
       </a>
-      <a href="https://github.com/Jpoliachik">
-        <GitHub className="svg-link w-5 h-5" fill={fillColor} />
-      </a>
       <a href="https://youtube.com/JustinPoliachik">
         <YouTube className="svg-link w-5 h-5" fill={fillColor} />
+      </a>
+      <a href="https://www.tiktok.com/@j_poli">
+        <TikTok className="svg-link w-5 h-5" fill={fillColor} />
+      </a>
+      <a href="https://github.com/Jpoliachik">
+        <GitHub className="svg-link w-5 h-5" fill={fillColor} />
       </a>
       <a href="https://linkedin.com/in/justinpoliachik/">
         <LinkedIn className="svg-link w-5 h-5" fill={fillColor} />
       </a>
-      <a href={resumeUrl}>
-        <Document className="svg-link w-5 h-5" fill={fillColor} />
-      </a>
+      {props.showResume && (
+        <a href={resumeUrl}>
+          <Document className="svg-link w-5 h-5" fill={fillColor} />
+        </a>
+      )}
     </div>
   );
 };

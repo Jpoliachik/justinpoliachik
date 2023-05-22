41dca72d3fb1188a63253c6ecfa1de820c5e20d4
Justin Poliachik <jpoliachik@gmail.com>
2023-05-22 09:15:53 -0400

    add explained in 20 seconds to projects list

--- components/MainFooter.tsx
+++ components/MainFooter.tsx
@@ -7,7 +7,7 @@ export const MainFooter = () => {
       <div className="text-xs text-gray-400 mr-8">@ Justin Poliachik ~ all of the years ~</div>
       <SocialLinks showResume={true} />
       <a className="ml-4 text-sm font-semibold text-green-700 no-underline hover:underline" href={resumeUrl}>
-        CV (2022)
+        CV (2023)
       </a>
     </div>
   );

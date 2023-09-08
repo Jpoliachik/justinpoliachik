47cfd525407592b3afd5d4cce90eb423b7083eff
Justin Poliachik <jpoliachik@gmail.com>
2023-09-08 09:21:35 -0400

    update resume url

--- lib/links.ts
+++ lib/links.ts
@@ -1,1 +1,1 @@
-export const resumeUrl = "https://jpoliachik-public.s3.amazonaws.com/documents/JustinPoliachik_CV_compressed.pdf";
+export const resumeUrl = "https://jpoliachik-public.s3.amazonaws.com/documents/JustinPoliachik_CV.pdf";

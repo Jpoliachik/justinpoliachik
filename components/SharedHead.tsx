ee1151feeea8808e554aa1541091c12bb915b9b3
Justin Poliachik <jpoliachik@gmail.com>
2024-10-27 14:50:07 -0400

    remove head description

--- components/SharedHead.tsx
+++ components/SharedHead.tsx
@@ -8,7 +8,6 @@ export const SharedHead = (props: { title: string }) => {
         rel="icon"
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦊</text></svg>"
       />
-      <meta name="description" content="Justin Poliachik is a software engineer." />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
   );

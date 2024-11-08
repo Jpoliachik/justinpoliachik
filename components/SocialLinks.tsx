9022f9c0c190b30ca11ce4dd744c32f5e73a3c37
Justin Poliachik <jpoliachik@gmail.com>
2024-11-08 11:20:27 -0500

    update social links, add bluesky

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,34 +1,30 @@
-import Twitter from "../icons/twitter.svg";
+import Bluesky from "../icons/bluesky.svg";
 import GitHub from "../icons/github.svg";
 import YouTube from "../icons/youtube.svg";
 import LinkedIn from "../icons/linkedin.svg";
-import TikTok from "../icons/tiktok.svg";
 import Document from "../icons/document.svg";
 import { resumeUrl } from "../lib/links";
 
 const fillColor = "#262626";
 
-export const SocialLinks = (props: { showResume?: boolean }) => {
+export const SocialLinks = (props: { showResume?: boolean; showGithub?: boolean; showLinkedIn?: boolean }) => {
   return (
     <div className="flex flex-row space-x-4">
-      <a href="https://twitter.com/JPoliachik">
-        <Twitter className="svg-link w-5 h-5" fill={fillColor} />
+      <a href="https://bsky.app/profile/justinpoliachik.com">
+        <Bluesky className="svg-link w-6 h-6" fill={fillColor} />
       </a>
       <a href="https://youtube.com/JustinPoliachik">
-        <YouTube className="svg-link w-5 h-5" fill={fillColor} />
-      </a>
-      <a href="https://www.tiktok.com/@j_poli">
-        <TikTok className="svg-link w-5 h-5" fill={fillColor} />
+        <YouTube className="svg-link w-6 h-6" fill={fillColor} />
       </a>
       <a href="https://github.com/Jpoliachik">
-        <GitHub className="svg-link w-5 h-5" fill={fillColor} />
+        <GitHub className="svg-link w-6 h-6" fill={fillColor} />
       </a>
       <a href="https://linkedin.com/in/justinpoliachik/">
-        <LinkedIn className="svg-link w-5 h-5" fill={fillColor} />
+        <LinkedIn className="svg-link w-6 h-6" fill={fillColor} />
       </a>
       {props.showResume && (
         <a href={resumeUrl}>
-          <Document className="svg-link w-5 h-5" fill={fillColor} />
+          <Document className="svg-link w-6 h-6" fill={fillColor} />
         </a>
       )}
     </div>
